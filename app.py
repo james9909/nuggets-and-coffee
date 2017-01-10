87c51ec97b5728d76fb6975125ba47e9d25f0abc
Sharon Lin <sharonlin14@gmail.com>
2017-01-10 12:53:18 -0500

    Added routes

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask
+import hashlib, os, json, random
+import sqlite3
+from flask import Flask, render_template, session, request, redirect, url_for
+import urllib, math, sys
+from itertools import count, groupby
 
 app = Flask(__name__)
 
+app.secret_key = os.urandom(32)
+secret = 'secret_cookie_key'
+
+#========================================ROUTAGE
+
+#either shows user their home (stories edited by them), or redirect to login
 @app.route("/")
 def index():
-    return ""
+    if (secret in session):
+        name = session[secret]
+        return render_template('index.html')
+    return render_template('base.html')
 
 if __name__ == "__main__":
     app.debug = True
