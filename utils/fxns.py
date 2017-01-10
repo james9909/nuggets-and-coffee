f9a81797c762a9e253ad8818f1072276e32345b9
Sharon Lin <sharonlin14@gmail.com>
2017-01-10 13:09:18 -0500

    Added accessor method

--- utils/fxns.py
+++ utils/fxns.py
@@ -1,1 +1,15 @@
 import sqlite3
+
+db = sqlite3.connect("data/dab.db", check_same_thread=False)
+d = db.cursor()
+
+# =========== START ACCESSOR METHODS =============
+
+def get_username(userid):
+    q = 'SELECT username FROM users WHERE id =\"%s\";' % (userid)
+    d.execute(q)
+    r = d.fetchall()
+    
+    return r[0][0]
+
+# ========== END ACCESSOR METHODS ================
