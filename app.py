347a1dc74fb25793c6bd5ba7293aeb88ad0fbdb3
James Wang <jameswang9909@hotmail.com>
2017-01-11 13:56:04 -0500

    Generate secret key if it doesn't exist

--- app.py
+++ app.py
@@ -46,5 +46,14 @@ def register():
     return render_template('login.html', action='register')
 
 if __name__ == "__main__":
+    # Generate and store secret key if it doesn't exist
+    with open(".secret_key", "a+b") as f:
+        secret_key = f.read()
+        if not secret_key:
+            secret_key = os.urandom(64)
+            f.write(secret_key)
+            f.flush()
+        app.secret_key = secret_key
+
     app.debug = True
     app.run()
