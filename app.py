e89498c9065d669c4c370ed814af3f717da5b2ad
JiaqiGao <jiaqigao12@gmail.com>
2017-01-11 07:07:29 -0500

    Added foursquare api

--- app.py
+++ app.py
@@ -3,17 +3,23 @@ import sqlite3
 from flask import Flask, render_template, session, request, redirect, url_for
 import urllib, math, sys
 from itertools import count, groupby
+import foursquare
 
 app = Flask(__name__)
 
 app.secret_key = os.urandom(32)
 secret = 'secret_cookie_key'
 
+client = foursquare.Foursquare(client_id='IVAQCEMVQ3OR00SDOCEZR4AQ5KEQXXRWKQYRAHLIVM50QWKK', client_secret='JGOJZECQYXHNPVSIH4WK2N5HTNECAJAWFL3RF2E5J03IZRNL')
+
+Lnuggets = client.venues.search(params={'query': 'chicken nuggets'})
 #========================================ROUTAGE
 
 #either shows user their home (stories edited by them), or redirect to login
 @app.route("/")
 def index():
+    print(client.user())
+    print(Lnuggets[:10])
     if (secret in session):
         name = session[secret]
         return render_template('index.html')
