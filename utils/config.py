9cfe9c3159b4d2c37ff2bac22ad3de56b1eb00b0
James Wang <jameswang9909@hotmail.com>
2017-01-26 01:29:29 -0500

    Read api keys from 2 locations

--- utils/config.py
+++ utils/config.py
@@ -4,8 +4,18 @@ def load_keys():
     global keys
     keys = {}
 
+    # Backwards compatibility
     if os.path.exists("utils/.keys"):
         f = open("utils/.keys", "r")
+        for line in f:
+            if "=" in line:
+                line = line.strip().split("=")
+                key, value = line[0], line[1]
+                print "Loading key \"%s\"..." % key
+                keys[key] = value
+
+    if os.path.exists(".keys"):
+        f = open(".keys", "r")
         for line in f:
             if "=" in line:
                 line = line.strip().split("=")
